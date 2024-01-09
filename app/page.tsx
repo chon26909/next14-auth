505b358b61156f84eeac5929ae8f4cd45e3bb7ca
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-09 22:00:31 +0700

    init project

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+import React from 'react';
+
+const font = Poppins({
+    subsets: ['latin'],
+    weight: ['600']
+});
+
+const page = () => {
+    return (
+        <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+            <div className='space-y-6 text-center'>
+                <h1 className={cn('text-6xl font-semibold text-white drop-shadow-md', font.className)}>🔐 Auth</h1>
+                <p className='text-white text-lg'>A simple authentication service</p>
+                <div>
+                    <Button variant='secondary' size='lg'>
+                        Sign In
+                    </Button>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default page;
