b27df6b6da812772397442d7344cb87e8f125ffb
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-20 23:08:00 +0700

    update login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,43 @@
 'use client';
-import React from 'react';
+import React, { useState, useTransition } from 'react';
 import CardWrapper from './card-wrapper';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { LoginSchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { FormError } from '../form-error';
+import { FormSuccess } from '../form-success';
+import { login } from '@/actions/login';
 
 const LoginForm = () => {
+    const [isPending, startTransition] = useTransition();
+    const [error, setError] = useState<string | undefined>('');
+    const [success, setSuccess] = useState<string | undefined>('');
+
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: 'chon@gmail.com',
+            password: '1234'
+        }
+    });
+
+    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+        setSuccess('');
+        setError('');
+
+        startTransition(async () => {
+            try {
+                const res = await login(values);
+                setSuccess(res.success);
+                setError(res.error);
+            } catch (error) {}
+        });
+    };
+
     return (
         <CardWrapper
             headerLabel='Welcome back'
@@ -10,7 +45,53 @@ const LoginForm = () => {
             backButtonHref='/auth/register'
             showSocial
         >
-            <div>login form</div>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className='space-y-4'>
+                        <FormField
+                            control={form.control}
+                            name='email'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel> Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder=''
+                                            type='email'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='password'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder=''
+                                            type='password'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        {success && <FormSuccess message={success} />}
+                        {error && <FormError message={error} />}
+                        <Button type='submit' className='w-full' disabled={isPending}>
+                            Login
+                        </Button>
+                    </div>
+                </form>
+            </Form>
         </CardWrapper>
     );
 };
