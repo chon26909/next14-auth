409b10783fcf8a16fe85619d850481487d9b71fe
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-20 23:07:50 +0700

    demo route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,12 @@
-export async function GET() {
-    const res = await fetch('https://dummyjson.com/products');
-    const data = await res.json();
+import { getProductList } from '@/database/product';
 
-    return Response.json({ data });
+export async function GET(req: Request, res: Response) {
+    try {
+        const products = await getProductList();
+        console.log('products :', products);
+        return Response.json({ message: 'success', data: products });
+    } catch (error) {
+        console.log('error', error);
+        return Response.json({ message: 'error', data: error });
+    }
 }
