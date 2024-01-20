e0b82d70360b0d03f4426e22c81103574ea67566
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-20 23:08:29 +0700

    social component

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,7 +1,21 @@
+'use client';
 import React from 'react';
 
-const social = () => {
-    return <div>social</div>;
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+import { Button } from '@/components/ui/button';
+
+const Social = () => {
+    return (
+        <div className='flex items-center w-full gap-x-2'>
+            <Button size='lg' className='w-full' variant='outline' onClick={() => {}}>
+                <FcGoogle className='h-5 w-5' />
+            </Button>
+            <Button size='lg' className='w-full' variant='outline' onClick={() => {}}>
+                <FaGithub className='h-5 w-5' />
+            </Button>
+        </div>
+    );
 };
 
-export default social;
+export default Social;
