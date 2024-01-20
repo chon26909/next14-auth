2137482a9ebaee349a752f390e5f0368185faaa3
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-20 23:07:41 +0700

    complete register action

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { getUserByEmail } from '@/database/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     console.log('values', values);
@@ -10,5 +13,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: 'Invalid fields!' };
     }
+
+    const { email, password, name } = validatedFields.data;
+
+    // bcrypt generate salt
+    const salt = await bcrypt.genSalt();
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: 'Email already exists!' };
+    }
+
+    await db.user.create({
+        data: {
+            email: email,
+            hash: hashedPassword,
+            salt: salt,
+            name: name
+        }
+    });
+
     return { success: 'Email sent!' };
 };
