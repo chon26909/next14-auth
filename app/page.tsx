df10eadc95a4c9c8b0082e75a491d8eb88d885d2
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-22 22:30:21 +0700

    index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import LoginButton from '@/components/auth/login-button';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { Poppins } from 'next/font/google';
@@ -9,16 +10,28 @@ const font = Poppins({
 });
 
 const page = () => {
+    const res = fetch('/api/hello');
+
+    console.log('res', res);
+
     return (
         <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
             <div className='space-y-6 text-center'>
-                <h1 className={cn('text-6xl font-semibold text-white drop-shadow-md', font.className)}>🔐 Auth</h1>
+                <h1
+                    className={cn(
+                        'text-6xl font-semibold text-white drop-shadow-md',
+                        font.className
+                    )}
+                >
+                    🔐 Auth
+                </h1>
                 <p className='text-white text-lg'>A simple authentication service</p>
-                <div>
+
+                <LoginButton>
                     <Button variant='secondary' size='lg'>
                         Sign In
                     </Button>
-                </div>
+                </LoginButton>
             </div>
         </main>
     );
