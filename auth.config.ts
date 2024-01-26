c11785e759fa1785b4be4932fbe32af26b71195f
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-26 22:35:26 +0700

    auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,27 @@
-import Github from 'next-auth/providers/github';
+// import Github from 'next-auth/providers/github';
+import bcrypt from 'bcrypt';
 import type { NextAuthConfig } from 'next-auth';
+import credentials from 'next-auth/providers/credentials';
+import { LoginSchema } from './schemas';
+import { getUserByEmail } from './database/user';
 
 export default {
-    providers: [Github]
+    providers: [
+        credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
+                    const user = await getUserByEmail(email);
+                    if (!user || !user.hash) return null;
+                    // const hashPassword = await bcrypt.hash(password, user.salt);
+                    // const passwordMatch = await bcrypt.compare(hashPassword, user.hash);
+                    // if (passwordMatch) return user;
+
+                    return user;
+                }
+                return null;
+            }
+        })
+    ]
 } satisfies NextAuthConfig;
---
+++ auth.ts
@@ -0,0 +1,71 @@
+import NextAuth, { Session } from 'next-auth';
+
+import authConfig from '@/auth.config';
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { db } from './lib/db';
+import { JWT } from 'next-auth/jwt';
+import { getUserById } from './database/user';
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut
+} = NextAuth({
+    session: {
+        strategy: 'jwt'
+    },
+    callbacks: {
+        async session({ token, session }: any) {
+            console.log('session', session);
+
+            console.log('token', token);
+
+            // if (session.user) {
+            //     session.user.email = '';
+            //
+
+            if (session.user) {
+                session.user.id = token.sub;
+            }
+
+            return Promise.resolve(session);
+        },
+        async jwt({ token }) {
+            console.log(token);
+
+            if (!token.sub) {
+                return token;
+            }
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) {
+                return token;
+            }
+
+            token.role = existingUser.role;
+
+            return token;
+        }
+    },
+    adapter: PrismaAdapter(db),
+    ...authConfig
+
+    // providers: [
+    //     github({
+    //         clientId: process.env.GITHUB_CLIENT_ID,
+    //         clientSecret: process.env.GITHUB_CLIENT_SECRET
+    //     })
+    // ]
+    // callbacks: {
+    //     async session(session, token) {
+    //         session.accessToken = token.accessToken;
+    //         return Promise.resolve(session);
+    //     },
+    //     async jwt(token, user, account, profile, isNewUser) {
+    //         return Promise.resolve(token);
+    //     }
+    // }
+});
