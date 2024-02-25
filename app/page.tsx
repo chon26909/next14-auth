dd81b50be8f8dd971ddfb504274021d9288b06e2
HiChon <59905927+chon26909@users.noreply.github.com>
2024-02-25 23:26:30 +0700

    remove /api/hello

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,6 @@ const font = Poppins({
 });
 
 const page = () => {
-    const res = fetch('/api/hello');
-
-    console.log('res', res);
-
     return (
         <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
             <div className='space-y-6 text-center'>
